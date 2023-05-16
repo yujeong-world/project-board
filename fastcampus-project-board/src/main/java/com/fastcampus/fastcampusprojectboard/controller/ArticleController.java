2139d00e070fd0370072081a15136e4d3ab90163
yujeong-world <kyjii082@gmail.com>
2023-05-16 21:00:56 +0900

    #16- 게시판 뷰 기본 골격 작성
    컨트롤러가 핸들러 메소드러 테스트를 통과하게 열어 줌
    뷰 적절히 뼈대 잡음
    테스트에서는 modal type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 테스트 메소드 수정

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
@@ -1,7 +1,12 @@
 package com.fastcampus.fastcampusprojectboard.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
 
 /*
 /articles
@@ -14,4 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }

236a596bfa0959e5d3a6481c1ef76ba71fc3d562
yujeong-world <kyjii082@gmail.com>
2023-05-21 10:56:58 +0900

    #28 - 페이지네이션 서비스 구현 및 테스트
    스프링 서비스 빈 형태로 설계하여
    현재 페이지와 총 페이지 수를 넣어주면 
    페이지 바에 사용할 번호 리스트를 반환하게끔 설계

---
+++ fastcampus-project-board/src/test/java/com/fastcampus/fastcampusprojectboard/service/PaginationServiceTest.java
@@ -0,0 +1,66 @@
+package com.fastcampus.fastcampusprojectboard.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("비즈니스 로직 - 페이지네이션")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = Void.class)
+class PaginationServiceTest {
+
+    private final PaginationService sut;
+    public PaginationServiceTest(@Autowired PaginationService paginationService) {
+        this.sut=paginationService;
+    }
+
+    @DisplayName("현재 페이지 번호와 총 페이지 수를 주면, 페이징 바 리스트를 만들어 준다.")
+    @MethodSource
+    @ParameterizedTest(name = "[{index}] 현재 페이지: {0},총 페이지: {1} =>{2}")
+    void givenCurrentPageNumberAndTotalPages_whenCalculating_thenReturnPaginationBarNumbers(int currentPageNumber, int totalPages, List<Integer> expected){
+        // given
+
+        //When
+        List<Integer> actual = sut.getPaginationBarNumbers(currentPageNumber, totalPages);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+
+
+    }
+
+    static Stream<Arguments> givenCurrentPageNumberAndTotalPages_whenCalculating_thenReturnPaginationBarNumbers(){
+        return Stream.of(
+          Arguments.arguments(0,13,List.of(0,1,2,3,4)),
+          Arguments.arguments(1,13,List.of(0,1,2,3,4)),
+          Arguments.arguments(2,13,List.of(0,1,2,3,4)),
+          Arguments.arguments(3,13,List.of(1,2,3,4,5)),
+          Arguments.arguments(4,13,List.of(2,3,4,5,6)),
+          Arguments.arguments(5,13,List.of(3,4,5,6,7)),
+          Arguments.arguments(6,13,List.of(4,5,6,7,8)),
+          Arguments.arguments(10,13,List.of(8,9,10,11,12)),
+          Arguments.arguments(11,13,List.of(9,10,11,12)),
+          Arguments.arguments(12,13,List.of(10,11,12))
+        );
+    }
+
+    @DisplayName("현재 설정되어 있는 페이지네이션 바의 길이를 알려준다.")
+    @Test
+    void givenNothing_whenCalling_thenReturnsCurrentBarLength(){
+        //Given
+
+        //When
+        int barLength = sut.currentBarLength();
+        //Then
+        assertThat(barLength).isEqualTo(5);
+    }
+
+}
